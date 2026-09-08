ed4de33e09ddd400756d75a0cece75b23249cf51
isabellesantana-debug <isabelle.santana@estudante.ifms.edu.br>
2026-09-08 01:55:07 +0000

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,32 @@
-import { useEffect, useState } from "react";
+async function buscarUsuarios() {
+  try {
+    setCarregando(true);
+    setErro("");
 
-function App() {
-  const [usuarios, setUsuarios] = useState([]);
-  const [carregando, setCarregando] = useState(true);
-  const [erro, setErro] = useState("");
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  useEffect(() => {
-    const controller = new AbortController();
-
-    async function buscarUsuarios() {
-      try {
-        setCarregando(true);
-        setErro("");
-
-        const resposta = await fetch(
-          "https://jsonplaceholder.typicode.com/usuariosenterrado",
-          {
-            signal: controller.signal,
-          }
-        );
-
-        if (!resposta.ok) {
-          throw new Error(`HTTP ${resposta.status}`);
-        }
-
-        const data = await resposta.json();
-
-        setUsuarios(data);
-      } catch (error) {
-        if (error.name === "AbortError") {
-          console.log("Requisição cancelada.");
-          return;
-        }
-
-        setErro(error.message);
-      } finally {
-        setCarregando(false);
+    const resposta = await fetch(
+      "https://jsonplaceholder.typicode.com/users",
+      {
+        signal: controller.signal,
       }
+    );
+
+    if (!resposta.ok) {
+      throw new Error(`HTTP ${resposta.status}`);
     }
 
-    buscarUsuarios();
+    const data = await resposta.json();
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
+    setUsuarios(data);
+  } catch (error) {
+    if (error.name === "AbortError") {
+      console.log("Requisição cancelada.");
+      return;
+    }
 
-  if (carregando) {
-    return <h2>Carregando...</h2>;
+    setErro(error.message);
+  } finally {
+    setCarregando(false);
   }
-
-  if (erro) {
-    return <h2>Erro: {erro}</h2>;
-  }
-
-  if (usuarios.length === 0) {
-    return <h2>Nenhum usuário encontrado.</h2>;
-  }
-
-  return (
-    <div>
-      <h1>Usuários</h1>
-
-      <ul>
-        {usuarios.slice(0, 10).map((usuario) => (
-          <li key={usuario.id}>{usuario.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
 }
-
-export default App;
